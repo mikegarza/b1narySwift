0eebc0a5d8148dc03e683cf4346dcd0d859b00cd
Michael Garza <mikeg@oceanhousemedia.com>
2019-01-04 02:41:04 -0800

    End of day 1. Created TabBarController and BinaryController. Started the UI for BinaryController, buttons are created, but there is an issue with constraints and rounding corners.

---
+++ b1nary/b1nary/Features/General/Controllers/HomeTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeTabBarController.swift
+//  b1nary
+//
+//  Created by Michael Garza on 1/3/19.
+//  Copyright © 2019 Michael Garza. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabBarController: UITabBarController {
+	
+}
+
+
