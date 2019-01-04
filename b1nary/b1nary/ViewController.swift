dcf131af7de9d25b5fba70fa6cd186de6b7a35d8
Michael Garza <mikeg@oceanhousemedia.com>
2019-01-03 21:38:09 -0800

    Initial project creation.

---
+++ b1nary/b1nary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  b1nary
+//
+//  Created by Michael Garza on 1/3/19.
+//  Copyright © 2019 Michael Garza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
